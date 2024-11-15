1200ac89297ccaf5c9981c1ebcfafad33b628df1
mbsimmon <marksim747@gmail.com>
2024-11-15 08:44:06 -0600

    cleaned up ThreePhase.cc to not include functions and #include statements that are irrelevant to the project

--- ThreePhase.cc
+++ ThreePhase.cc
@@ -1,36 +1,21 @@
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/sparse_matrix.h>
-#include <deal.II/lac/sparse_direct.h>
 #include <deal.II/distributed/shared_tria.h>
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_in.h>
 #include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/grid_refinement.h>
-#include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/fe/fe_values.h>
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/numerics/vector_tools.h>
-#include <deal.II/dofs/dof_accessor.h>
 #include <deal.II/dofs/dof_tools.h>
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/fe/mapping_q1.h>
 #include <deal.II/fe/fe_dgq.h>
-#include <deal.II/fe/fe_dg_vector.h>
-#include <deal.II/fe/fe_interface_values.h>
-#include <deal.II/lac/solver_richardson.h>
-#include <deal.II/lac/solver_gmres.h>
-#include <deal.II/lac/solver_cg.h>
-#include <deal.II/lac/precondition.h>
 #include <deal.II/lac/precondition_block.h>
-#include <deal.II/numerics/derivative_approximation.h>
 #include <deal.II/lac/affine_constraints.h>
-#include <deal.II/numerics/matrix_tools.h>
 #include <deal.II/base/tensor_product_polynomials.h>
-#include <deal.II/base/polynomial.h>
-#include <deal.II/fe/fe_face.h>
 #include <deal.II/fe/fe_raviart_thomas.h>
 
 #include <deal.II/base/conditional_ostream.h>
@@ -39,13 +24,9 @@
 #include <deal.II/base/timer.h>
 
 #include <deal.II/grid/grid_tools.h>
-#include <deal.II/dofs/dof_renumbering.h>
 
-#include <deal.II/meshworker/mesh_loop.h>
-#include <deal.II/meshworker/scratch_data.h>
 #include <deal.II/base/parameter_handler.h>
 
-#include "AverageGradientOperators.hh"
 
 #include "RT_projection.hh"
 #include "pl_problem.hh"
@@ -55,9 +36,7 @@
 
 // PETSc stuff
 #include <deal.II/lac/petsc_vector.h>
-#include <deal.II/lac/petsc_sparse_matrix.h>
 #include <deal.II/lac/petsc_solver.h>
-#include <deal.II/lac/petsc_precondition.h>
 
 
 #include <cstdlib>
@@ -69,36 +48,11 @@
 namespace CouplingPressureSaturation {
     using namespace dealii;
 
-    struct CopyDataFace {
-        FullMatrix<double> cell_matrix;
-        Vector<double> cell_rhs;
-        std::vector<types::global_dof_index> joint_dof_indices;
-        std::array<unsigned int, 2> cell_indices;
-        std::array<double, 2> values;
-    };
 
-
-    struct CopyData {
-        FullMatrix<double> cell_matrix;
-        Vector<double> cell_rhs;
-        std::vector<types::global_dof_index> local_dof_indices;
-        std::vector<CopyDataFace> face_data;
-        double value;
-        unsigned int cell_index;
-
-        template<class Iterator>
-        void reinit(const Iterator &cell, unsigned int dofs_per_cell) {
-            cell_matrix.reinit(dofs_per_cell, dofs_per_cell);
-            cell_rhs.reinit(dofs_per_cell);
-
-            local_dof_indices.resize(dofs_per_cell);
-            cell->get_dof_indices(local_dof_indices);
-        }
-    };
 
     class ParameterReader : public Subscriptor {
     public:
-        ParameterReader(ParameterHandler &);
+        explicit ParameterReader(ParameterHandler &);
 
         void read_parameters(const std::string &);
 
@@ -336,8 +290,8 @@ namespace CouplingPressureSaturation {
     template<int dim>
     class CoupledPressureSaturationProblem {
     public:
-        CoupledPressureSaturationProblem(const unsigned int degree, const unsigned int degreeRT_,
-                                         double delta_t, double tf, const unsigned int refinement,
+        CoupledPressureSaturationProblem(unsigned int degree, unsigned int degreeRT_,
+                                         double delta_t, double tf, unsigned int refinement,
                                          ParameterHandler &param);
 
         void run();
@@ -363,7 +317,6 @@ namespace CouplingPressureSaturation {
 
         double calculate_energy(double &real_energy);
 
-        using ScratchData = MeshWorker::ScratchData<dim>;
 
         parallel::shared::Triangulation<dim> triangulation;
 
@@ -449,7 +402,6 @@ namespace CouplingPressureSaturation {
 
         bool incompressible;
 
-        bool Stab_t;
         bool Stab_a;
         bool Stab_v;
 
