e167741053e7082a08d64ef583fd3f43f8800d50
Msimm007 <marksim747@gmail.com>
2025-09-25 09:07:27 -0500

    debugged stab sv for the most part. Will merge for now

--- QUARTER_5_SPOT/aux_primary.cc
+++ QUARTER_5_SPOT/aux_primary.cc
@@ -7,4 +7,4 @@ double stab_sa_data = 12000.0;
 double stab_sv_data = 5.0;
 
 // if true, makes kappa_abs 1000 times lower in the region [25, 50] x [25, 50] (x [25, 50] if 3d)
-bool hetero = false;
+bool hetero = true;
