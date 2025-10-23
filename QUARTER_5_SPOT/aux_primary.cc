0789790062f5b9c75a5aa444ff82c828e8fe07e3
Msimm007 <marksim747@gmail.com>
2025-10-23 08:48:48 -0500

    sims: running to see if lower stab parameter is the soluton. running when stab sa is 500

--- QUARTER_5_SPOT/aux_primary.cc
+++ QUARTER_5_SPOT/aux_primary.cc
@@ -3,8 +3,8 @@
 double amp_factor_cap_pressure = 1.e7;
 
 double stab_pl_data = 12000.0;
-double stab_sa_data = 12000.0;
+double stab_sa_data = 500.0;
 double stab_sv_data = 5.0;
 
 // if true, makes kappa_abs 1000 times lower in the region [25, 50] x [25, 50] (x [25, 50] if 3d)
-bool hetero = true;
+bool hetero = false;
